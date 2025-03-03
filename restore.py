14457d87d83e4a2cbdd7f3a03b313ed21a1a71ad
Alex Antonchev <antontchev@gmail.com>
2025-03-03 06:51:57 +0400

    add logging function

--- restore.py
+++ restore.py
@@ -11,6 +11,7 @@ import subprocess
 import sys
 import csv
 import glob
+import logging
 
 
 def mount_dir(mount_command):
@@ -22,12 +23,12 @@ def mount_dir(mount_command):
             stderr=subprocess.PIPE,
             check=True
         )
-        print('INFO: Mount success')
+        logger.info('Mount success.')
     except KeyboardInterrupt:
-        print('KeyboardInterrupt')
+        logger.warning('KeyboardInterrupt.')
     except subprocess.CalledProcessError as err:
-        print(f'ERR: Error mount {mount_point}')
-        print(err.__str__())
+        logger.error(f'Error mount {mount_point}')
+        logger.error(err.__str__())
         sys.exit(2)
 
 def get_snapshot_dates(snapshot_dir):
@@ -40,34 +41,49 @@ def unpack_archive(backup_file, restore_dir):
 mount_point = '/mnt/store/'
 backup_dir = f'{mount_point}Backup/test/'
 restore_dir = '/media/antonchev/USB-накопитель/test_restore/'
+log_file = 'backup.log'
 snapshot_dir = f'{backup_dir}snapshots/'
 mount_command = ['sudo', 'mount', '-U', '01DB2557286B1350', mount_point, '-o', 'uid=1000,gid=1000']
 
+formatter = logging.Formatter(fmt='%(asctime)s: %(name)s: %(levelname)s - %(message)s',
+                              datefmt='%Y-%m-%d %H:%M:%S')
+
+console = logging.StreamHandler()
+console.setFormatter(formatter)
+
+file = logging.FileHandler(filename=log_file, mode='a')
+file.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(console)
+logger.addHandler(file)
+
 if not os.path.ismount(mount_point):
-    print('INFO: Mount point not exists')
+    logger.info('INFO: Mount point not exists, mounting.')
     mount_dir(mount_command)    
 
 if len(sys.argv) == 1:
     shapshots_dates = get_snapshot_dates(snapshot_dir)
 elif sys.argv[1] not in get_snapshot_dates(snapshot_dir):
-    print(f'ERROR: Incorrect restore date {sys.argv[1]}')
+    logger.error(f'Incorrect restore date {sys.argv[1]}.')
     sys.exit(2)
 else:
     shapshots_dates = [el for el in get_snapshot_dates(snapshot_dir) if el <= sys.argv[1]]
 
 pathlib.Path(restore_dir).mkdir(parents=True, exist_ok=True)
 
-print(f'INFO: restore on date {max(shapshots_dates)}')
+logger.info(f'Restore on date {max(shapshots_dates)}.')
 for date in shapshots_dates:
     tar_files_for_date = glob.glob(f'{backup_dir}{date}*.tar.gz')
     for file in tar_files_for_date:
-        print(f'INFO: Unpack file {file}')
+        logger.info(f'Unpack file {file}.')
         unpack_archive(file, restore_dir)   
     del_files_for_date = glob.glob(f'{backup_dir}{date}.deleted.csv')
     for file in del_files_for_date:
         with open(file, 'r', newline=None) as f:
             reader = csv.reader(f)
             for row in reader:
-                print(f'INFO: Delete file {row}')
+                logger.info(f'Delete file {row}.')
                 pathlib.Path(f'{restore_dir}{"".join(row)[1:]}').unlink()
-print(f'INFO: Unpacking is finished')
+logger.info(f'Restore is finished.')
