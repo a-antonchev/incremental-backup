b5e06a680747f13a3a54026ceba9f323466553f1
Alex Antonchev <antontchev@gmail.com>
2025-03-03 06:59:02 +0400

    style: apply PEP 8 formatting using autopep8

--- restore.py
+++ restore.py
@@ -31,8 +31,10 @@ def mount_dir(mount_command):
         logger.error(err.__str__())
         sys.exit(2)
 
+
 def get_snapshot_dates(snapshot_dir):
     return sorted([el.split('.')[0] for el in os.listdir(snapshot_dir)])
+
 
 def unpack_archive(backup_file, restore_dir):
     with tarfile.open(backup_file, 'r:gz') as f:
@@ -43,7 +45,8 @@ backup_dir = f'{mount_point}Backup/test/'
 restore_dir = '/media/antonchev/USB-накопитель/test_restore/'
 log_file = 'backup.log'
 snapshot_dir = f'{backup_dir}snapshots/'
-mount_command = ['sudo', 'mount', '-U', '01DB2557286B1350', mount_point, '-o', 'uid=1000,gid=1000']
+mount_command = ['sudo', 'mount', '-U', '01DB2557286B1350',
+                 mount_point, '-o', 'uid=1000,gid=1000']
 
 formatter = logging.Formatter(fmt='%(asctime)s: %(name)s: %(levelname)s - %(message)s',
                               datefmt='%Y-%m-%d %H:%M:%S')
@@ -60,7 +63,7 @@ logger.addHandler(console)
 logger.addHandler(file)
 
 if not os.path.ismount(mount_point):
-    logger.info('INFO: Mount point not exists, mounting.')
+    logger.info('Mount point not exists, mounting.')
     mount_dir(mount_command)    
 
 if len(sys.argv) == 1:
@@ -69,7 +72,8 @@ elif sys.argv[1] not in get_snapshot_dates(snapshot_dir):
     logger.error(f'Incorrect restore date {sys.argv[1]}.')
     sys.exit(2)
 else:
-    shapshots_dates = [el for el in get_snapshot_dates(snapshot_dir) if el <= sys.argv[1]]
+    shapshots_dates = [el for el in get_snapshot_dates(
+        snapshot_dir) if el <= sys.argv[1]]
 
 pathlib.Path(restore_dir).mkdir(parents=True, exist_ok=True)
 
@@ -78,7 +82,7 @@ for date in shapshots_dates:
     tar_files_for_date = glob.glob(f'{backup_dir}{date}*.tar.gz')
     for file in tar_files_for_date:
         logger.info(f'Unpack file {file}.')
-        unpack_archive(file, restore_dir)   
+        unpack_archive(file, restore_dir)
     del_files_for_date = glob.glob(f'{backup_dir}{date}.deleted.csv')
     for file in del_files_for_date:
         with open(file, 'r', newline=None) as f:
